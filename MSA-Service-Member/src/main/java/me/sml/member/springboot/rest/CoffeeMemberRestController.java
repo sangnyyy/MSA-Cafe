897e675d4eb5cb477bb1d509298488f799524674
SML <dltkdals2202@gmail.com>
2019-02-10 00:10:51 +0900

    feat : 에코시스템 관련 설정파일 적용

--- MSA-Service-Member/src/main/java/me/sml/member/springboot/rest/CoffeeMemberRestController.java
+++ MSA-Service-Member/src/main/java/me/sml/member/springboot/rest/CoffeeMemberRestController.java
@@ -1,7 +1,9 @@
 package me.sml.member.springboot.rest;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import me.sml.member.domain.repository.MemberRepository;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class CoffeeMemberRestController {
 
     private final MemberRepository memberRepository;
 
+    @HystrixCommand
     @GetMapping("/v1.0/{memberName}")
     public boolean isMember(@PathVariable("memberName") String memberName){
         if(memberRepository.findByName(memberName) != null){
@@ -25,8 +29,4 @@ public class CoffeeMemberRestController {
         }
     }
 
-    @GetMapping("/v1.0")
-    public boolean isMemberWithNoName(){
-        return true;
-    }
 }
